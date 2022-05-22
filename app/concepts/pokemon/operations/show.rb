5a045e408a7025bf436e33dc5f99290e51858fb7
Tomu Komatsu <tomu5642514@gmail.com>
2022-05-21 19:29:49 -0500

    Create Pokemon CRUD Operations

---
+++ app/concepts/pokemon/operations/show.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Pokemon
+  module Operations
+    class Show < Trailblazer::Operation
+      pass :set_pokemon
+      pass :render_json
+
+      def set_pokemon(ctx, pokemon_id:, **)
+        ctx[:pokemon] = Pokemon.find(pokemon_id)
+      end
+
+      def render_json(ctx, pokemon:, **)
+        ctx[:json] = Pokemon::Representers::Pokemon.jsonapi_new(pokemon).to_json
+      end
+    end
+  end
+end
