1205b1013d7cfa4c87d2a42bf1d28c8bb7206942
Tomu Komatsu <tomu5642514@gmail.com>
2022-05-21 19:35:59 -0500

    Create General and Doctor ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-  include ErrorHandler
+  # include ErrorHandler
+
+  # helper method to access the current user from the token
+  def current_user
+    @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
+  end
 end
