a91084b09e0e8d0357c6b28ea237568206c67e6e
Tomu Komatsu <tomu5642514@gmail.com>
2022-05-22 23:53:22 -0500

    Fix Mailer Tests

--- spec/mailers/captured_pokemon_spec.rb
+++ spec/mailers/captured_pokemon_spec.rb
@@ -2,14 +2,17 @@ require "rails_helper"
 
 RSpec.describe CapturedPokemonMailer, type: :mailer do
   describe "captured_pokemon_notification" do
-    let(:mail) { CapturedPokemonMailer.with(captured_pokemon_id: 1, user_id: 1).captured_pokemon_notification }
+    let(:mail) do
+      CapturedPokemonMailer.with(captured_pokemon_id: @captured_pokemon.id,
+                                 user_id: @user.id).captured_pokemon_notification
+    end
 
     it "renders the headers" do
-      user = create(:user)
+      @user = create(:user)
       pokemon = create(:pokemon)
-      create(:captured_pokemon, pokemon: pokemon, user: user)
+      @captured_pokemon = create(:captured_pokemon, pokemon: pokemon, user: @user)
       expect(mail.subject).to eq("New Captured Pokemon Notification")
-      expect(mail.to).to eq([user.email])
+      expect(mail.to).to eq([@user.email])
       expect(mail.from).to eq(["tkomatsu@applaudostudios.dev"])
     end
   end
--- spec/mailers/pokemon_spec.rb
+++ spec/mailers/pokemon_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 RSpec.describe PokemonMailer, type: :mailer do
   describe "new_pokemon_notification" do
-    let(:mail) { PokemonMailer.with(pokemon_id: 1).new_pokemon_notification }
+    let(:mail) { PokemonMailer.with(pokemon_id: @pokemon.id).new_pokemon_notification }
 
     it "renders the headers" do
       rand(1..5).times do
         create(:user)
       end
-      create(:pokemon)
+      @pokemon = create(:pokemon)
       expect(mail.subject).to eq("New Pokemon Notification")
       expect(mail.to).to eq(User.trainer_role.pluck(:email))
       expect(mail.from).to eq(["tkomatsu@applaudostudios.dev"])
